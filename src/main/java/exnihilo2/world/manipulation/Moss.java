259e4f7cb324bea1099e5a5f4e10beda9b3e35d6
Erasmus_Crowley <erasmus.crowley@gmail.com>
2015-04-29 05:16:15 -0500

    Inverted the moss spread logic to eliminate any possible lag that might have cropped up in ocean biomes.

--- src/main/java/exnihilo2/world/manipulation/Moss.java
+++ src/main/java/exnihilo2/world/manipulation/Moss.java
@@ -11,59 +11,46 @@ import net.minecraft.world.World;
 import net.minecraft.world.chunk.Chunk;
 
 public class Moss {
-	private static int speed = 16;
-	
+	private static int speed = 256;
+
 	public static void grow(World world, Chunk chunk)
 	{
 		for (int i = 0; i < speed; i++)
 		{
 			BlockPos pos = new BlockPos((chunk.xPosition * 16) + world.rand.nextInt(16), world.rand.nextInt(256), (chunk.zPosition * 16) + world.rand.nextInt(16));
 			IBlockState state = world.getBlockState(pos);
-			
-			if (state.getBlock() == Blocks.water || state.getBlock() == Blocks.mossy_cobblestone || (state.getBlock() == Blocks.stonebrick && state.getBlock().getMetaFromState(state) == 1))
+
+			if (state.getBlock() == Blocks.cobblestone || state.getBlock() == Blocks.stonebrick)
 			{
-				spreadToAdjacentBlocks(world, pos);
-			}
-		}
-	}
-	
-	private static void spreadToAdjacentBlocks(World world, BlockPos pos)
-	{
-		if (!trySpreadToBlock(world, new BlockPos(pos.getX() - 1, pos.getY(), pos.getZ())))
-		{
-			if (!trySpreadToBlock(world, new BlockPos(pos.getX() + 1, pos.getY(), pos.getZ())))
-			{
-				if (!trySpreadToBlock(world, new BlockPos(pos.getX(), pos.getY(), pos.getZ() - 1)))
+				if (hasAdjacentMossSources(world, pos))
 				{
-					if (!trySpreadToBlock(world, new BlockPos(pos.getX(), pos.getY(), pos.getZ() + 1)))
+					if (state.getBlock() == Blocks.cobblestone)
 					{
-						if (!trySpreadToBlock(world, new BlockPos(pos.getX(), pos.getY() - 1, pos.getZ())))
-						{
-							trySpreadToBlock(world, new BlockPos(pos.getX(), pos.getY() + 1, pos.getZ()));
-						}
+						world.setBlockState(pos, Blocks.mossy_cobblestone.getDefaultState());
+					}
+					else if (state.getBlock() == Blocks.stonebrick)
+					{
+						world.setBlockState(pos, Blocks.stonebrick.getDefaultState().withProperty(BlockStoneBrick.VARIANT, BlockStoneBrick.EnumType.MOSSY));
 					}
 				}
 			}
 		}
 	}
-	
-	private static boolean trySpreadToBlock(World world, BlockPos pos)
+
+	private static boolean hasAdjacentMossSources(World world, BlockPos pos)
 	{
-		boolean spread = false;
-		
+		return (isMossSource(world, new BlockPos(pos.getX() - 1, pos.getY(), pos.getZ()))
+				|| isMossSource(world, new BlockPos(pos.getX() + 1, pos.getY(), pos.getZ()))
+				|| isMossSource(world, new BlockPos(pos.getX(), pos.getY(), pos.getZ() - 1))
+				|| isMossSource(world, new BlockPos(pos.getX(), pos.getY(), pos.getZ() + 1))
+				|| isMossSource(world, new BlockPos(pos.getX(), pos.getY() - 1, pos.getZ()))
+				|| isMossSource(world, new BlockPos(pos.getX(), pos.getY() + 1, pos.getZ())));
+	}
+
+	private static boolean isMossSource(World world, BlockPos pos)
+	{
 		IBlockState state = world.getBlockState(pos);
 
-		if (state.getBlock() == Blocks.cobblestone)
-		{
-			world.setBlockState(pos, Blocks.mossy_cobblestone.getDefaultState());
-			spread = true;
-		}
-		else if (state.getBlock() == Blocks.stonebrick)
-		{
-			world.setBlockState(pos, Blocks.stonebrick.getDefaultState().withProperty(BlockStoneBrick.VARIANT, BlockStoneBrick.EnumType.MOSSY));
-			spread = true; 
-		}
-		
-		return spread;
+		return state.getBlock() == Blocks.water || state.getBlock() == Blocks.mossy_cobblestone || (state.getBlock() == Blocks.stonebrick && state.getBlock().getMetaFromState(state) == 1);
 	}
 }
