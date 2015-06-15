3a484e3674169b46453c8c23df3b951e9f6c1383
Erasmus_Crowley <erasmus.crowley@gmail.com>
2015-06-15 17:27:21 -0500

    Fixed an issue that caused hammers to not have a damage value.

--- src/main/java/exnihilo2/items/hammers/ItemHammer.java
+++ src/main/java/exnihilo2/items/hammers/ItemHammer.java
@@ -1,5 +1,9 @@
 package exnihilo2.items.hammers;
 
+import java.util.Set;
+
+import com.google.common.collect.Sets;
+
 import exnihilo2.EN2;
 import exnihilo2.registries.hammering.HammerRegistry;
 import net.minecraft.block.Block;
@@ -12,24 +16,30 @@ import net.minecraft.init.Blocks;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Item.ToolMaterial;
+import net.minecraft.item.ItemTool;
 import net.minecraft.util.BlockPos;
 import net.minecraft.world.World;
 
-public class ItemHammer extends Item{
-	
+public class ItemHammer extends ItemTool{
+	private final static Set EMPTY_SET = Sets.newHashSet(new Block[]{});
 	private final Item.ToolMaterial material;
 	
 	public ItemHammer(ToolMaterial material) {
+		super(3.0f, material, EMPTY_SET);
 		this.material = material;
 		this.maxStackSize = 1;
 		this.setMaxDamage((int)(material.getMaxUses()));
 		this.setCreativeTab(CreativeTabs.tabTools);
 	}
+	
+	
 
 	@Override
 	public float getDigSpeed(ItemStack itemstack, IBlockState state) 
 	{
-		if (HammerRegistry.isHammerable(state))
+		if (HammerRegistry.isHammerable(state) 
+				&& (state.getBlock().getMaterial().isToolNotRequired()
+				|| state.getBlock().getHarvestLevel(state) <= this.material.getHarvestLevel()))
 		{
 			return this.material.getEfficiencyOnProperMaterial() * 0.8f;
 		}
@@ -65,7 +75,8 @@ public class ItemHammer extends Item{
 	@Override
 	public boolean canHarvestBlock(Block block)
     {
-		return !block.equals(Blocks.air);
+		return (block.getMaterial().isToolNotRequired()
+				|| block.getHarvestLevel(block.getDefaultState()) <= this.material.getHarvestLevel());
     }
 	
 	@Override
